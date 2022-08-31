6441650033bb42617397776a0f4870c30cc42c00
Angel Zhou <angel.zhou1016@gmail.com>
2022-08-31 12:44:50 -0400

    adds generated color's rgb to the page

--- fancybutton.js
+++ fancybutton.js
@@ -1,4 +1,6 @@
 const button = document.querySelector('button');
+const firstColorText = document.getElementById('firstColor');
+const secondColorText = document.getElementById('secondColor');
 
 function generateRandomColor() {
     const r = Math.floor(Math.random() * 256);
@@ -6,11 +8,18 @@ function generateRandomColor() {
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
+
 function generateGradient() {
   const firstColor = generateRandomColor();
   const secondColor = generateRandomColor();
   const randomGradient = `linear-gradient(to bottom, ${firstColor}, ${secondColor})`
   document.body.style.background = randomGradient;
+  firstColorText.innerText = firstColor;
+  firstColorText.style.background = "rgba(255,255,255,.4)";
+  firstColorText.style.padding = "8px 8px 5px 8px";
+  secondColorText.innerText = secondColor;
+  secondColorText.style.background = "rgba(255,255,255,.4)";
+  secondColorText.style.padding = "8px 8px 5px 8px";
 }
 
 button.addEventListener("click", generateGradient);
